ac56ee7c488d234a2f7cb985fab0d7bc90cdc9b1
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-03-08 11:16:54 +0900

    Naver 뉴스앱

--- Part1/studyPyQt/NaverApi.py
+++ Part1/studyPyQt/NaverApi.py
@@ -6,7 +6,7 @@ import json # 결과는 json으로
 class NaverApi:
     # 생성자
     def __init__(self) -> None:
-        print('Naver API 생성')
+        print(f'[{datetime.datetime.now()}] Naver API 생성')
 
     # Naver API를 요청하는 함수
     def get_request_url(self, url):
@@ -18,13 +18,13 @@ class NaverApi:
         try:
             res = urlopen(req) # 요청 결과가 바로 돌아온다.
             if res.getcode() == 200: # response OK
-                print(f'[{datetime.datetime.now}] NaverAPI 요청 성공')
+                print(f'[{datetime.datetime.now()}] NaverAPI 요청 성공')
                 return res.read().decode('utf-8')
             else:
-                print(f'[{datetime.datetime.now}] NaverAPI 요청 실패')
+                print(f'[{datetime.datetime.now()}] NaverAPI 요청 실패')
                 return None
         except Exception as e:
-            print(f'[{datetime.datetime.now}] 예외 발생 : {e}')
+            print(f'[{datetime.datetime.now()}] 예외 발생 : {e}')
             return None
         
     # 실제 호출함수
@@ -41,16 +41,3 @@ class NaverApi:
             return None
         else:
             return json.loads(retData) # json으로 return.
-
-    # json으로 받은 데이터를 list로 변환
-    def get_post_data(self, post, outputs):
-        title = post['title']
-        description = post['description']
-        originallink = post['originallink']
-        link = post['link']
-
-        # 'Tue, 07 Mar 2023 15:44:00 +0900' 문자열로 들어온 걸 날짜형으로 변경
-        pDate = datetime.datetime.strptime(post['pobDate'], '%a, %d %b %Y %H:%M:%S +0900')
-        pubDate = pDate.strftime('%Y-%m-%d %H:%M:%S') # 2023-03-07 15:44:00 변경
-
-        # outputs에 옮기기
--- Part1/studyPyQt/mp03_naverSearchApp.py
+++ Part1/studyPyQt/mp03_naverSearchApp.py
@@ -3,18 +3,31 @@
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 from NaverApi import *
+import webbrowser # 웹 브라우저 모듈
+
 
 class qtApp(QWidget):
 
     def __init__(self):
         super().__init__()
         uic.loadUi('./Part1/studyPyQt/naverApiSearch.ui', self)
+        self.setWindowIcon(QIcon('./Part1/studyPyQt/newspaper.png'))
 
         # 검색 버튼 클릭시그널 / 슬롯함수
         self.btnSearch.clicked.connect(self.btnSearchClicked)
         # 텍스트 박스에 검색어 입력 후 엔터를 치면 처리
         self.txtSearch.returnPressed.connect(self.txtSearchReturned)
+        self.tblResult.doubleClicked.connect(self.tblResultDoubleClicked)
+
+    def tblResultDoubleClicked(self):
+        # row = self.tblResult.currentIndex().row()
+        # column = self.tblResult.currentIndex().column()
+        # print(row, column)
+        selected = self.tblResult.currentRow()
+        url = self.tblResult.item(selected, 1).text()
+        webbrowser.open(url) # 뉴스기사 웹사이트 오픈
 
     def btnSearchClicked(self):
         search = self.txtSearch.text()
@@ -28,15 +41,44 @@ class qtApp(QWidget):
             display = 100
             outputs = [] # 결과 담을 리스트 변수
             result = api.get_naver_search(node, search, 1, display)
-            # print(result) 개발 할 때만 쓰는거
-            # 리스트 뷰에 출력하는 기능
-            while result != None and result['display'] != 0:
-                for post in result['items']: # 100개의 post가 만들어진다.
-                    api.get_post_data(post, outputs) # NaverApi 클래스에서 처리
+
+            items = result['items'] # json결과 중에서 items 아래 배열만 추출
+            self.makeTable(items) # 테이블 위젯에 데이터들을 할당 함수    
 
     def txtSearchReturned(self):
-        self.btnSearchClicked()
+        self.btnSearchClicked() 
+            # print(result) 개발 할 때만 쓰는거
+            # 테이블 위젯에 출력하는 기능
 
+    # 테이블 위젯에 데이터 표시
+    def makeTable(self, items) -> None:
+        self.tblResult.setSelectionMode(QAbstractItemView.SingleSelection) # 단일 선택만 되게 만듦
+        self.tblResult.setColumnCount(2)
+        self.tblResult.setRowCount(len(items)) # 현재 100개 행 생성
+        self.tblResult.setHorizontalHeaderLabels(['기사제목','뉴스링크'])
+        # 컬럼 크기 조절
+        self.tblResult.setColumnWidth(0, 310)
+        self.tblResult.setColumnWidth(1, 260)
+        # 컬럼 데이터를 수정 금지
+        self.tblResult.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        for i, post in enumerate(items): # 0, 뉴스 ...
+            title = self.replaceHtmlTag(post['title']) # HTML 특수문자 변환
+            originallink = post['originallink']
+            # setItem(행, 열, 넣을 데이터)
+            self.tblResult.setItem(i, 0, QTableWidgetItem(title))
+            self.tblResult.setItem(i, 1, QTableWidgetItem(originallink))
+
+    def replaceHtmlTag(self, sentence) -> str:
+        result = sentence.replace('&lt;','<') # lesser then 작다
+        result = result.replace('&gt;', '>') # greater then 크다
+        result = result.replace('<b>', '') # bold
+        result = result.replace('</b>', '') # b
+        result = result.replace('&apos;', "'") # apostropy 홑따옴표
+        result = result.replace('&quot;', '"') # quotation mark 쌍따옴표
+        # 줄여서 써도 되고 늘려서 써도 되고
+        # 변환 안된 특수문자가 나타날 때 마다 추가
+        return result
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
