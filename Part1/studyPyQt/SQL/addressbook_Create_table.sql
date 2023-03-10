ceeec01ab8f5065a24fe1a82b869e88c1e6c03b7
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-03-10 14:08:33 +0900

    테이블 생성 쿼리

---
+++ Part1/studyPyQt/SQL/addressbook_Create_table.sql
@@ -0,0 +1,9 @@
+-- MySQL용
+CREATE TABLE addressbook (
+  Idx int NOT NULL AUTO_INCREMENT COMMENT 'PK 자동증가',
+  FullName varchar(50)  NOT NULL COMMENT '이름',
+  PhoneNum varchar(20) NOT NULL COMMENT '전화번호',
+  Email varchar(120) DEFAULT NULL COMMENT '이메일 Null 허용',
+  Address varchar(250)  DEFAULT NULL COMMENT '주소 Null 허용',
+  PRIMARY KEY (Idx)
+) COMMENT='주소록 테이블';
